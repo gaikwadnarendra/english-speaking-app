6e018031c165462650bc56799d229897171fdc0f
Narendra Gaikwad <narendragaikwad1419@gmail.com>
2026-09-13 15:20:29 +0530

    Fixed seed1

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { requireAuth } = require('../middleware/authMiddleware');
-//hey
+
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/me', requireAuth, authController.getMe);
